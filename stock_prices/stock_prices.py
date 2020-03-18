f7b20c099120aee2e01e84831e09f580e09f66bd
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-03-18 15:55:03 -0400

    eating cookies and stock prices attempted

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,28 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
-
+    # min_ = 0
+    # max_ = len(prices)-1
+    # for i in range(len(prices)-1):
+    #     if prices[min_] > prices[i]:
+    #         min_ = i
+    # for i in range(min_+1, len(prices)-1):
+    #     if prices[max_] < prices[i]:
+    #         max_ = i
+    # return prices[max_] - prices[min_]
+    min_value = None
+    for value in prices:
+        if not min_value:
+            min_value = value
+        elif value < min_value:
+            min_value = value
+    max_value = None
+    for value in prices:
+            if not max_value:
+                max_value = value
+            elif value < max_value:
+                max_value = value
+    return prices[min_value] - prices[max_value]
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
